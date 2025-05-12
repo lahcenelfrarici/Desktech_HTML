84dc84fa5c0194236c77d0f1e118fc1e23465694
Lahcen Elfarici <lahcen@fornet.ma>
2025-05-12 12:32:34 +0100

    add html

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -91,6 +91,29 @@
       items: 1,
       slideSpeed: 2000,
       nav: true,
+      navText: [
+        `<svg xmlns="http://www.w3.org/2000/svg" width="50" height="96" viewBox="0 0 50 96" fill="none">
+        <g clip-path="url(#clip0_11_98)">
+          <path d="M37.0075 84.202L13.0743 48.7677L37.2815 13.52" stroke="#E7E7E7" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        </g>
+        <defs>
+          <clipPath id="clip0_11_98">
+            <rect width="94.2433" height="48.1409" fill="white" transform="translate(0.859375 95.0557) rotate(-89.7779)"/>
+          </clipPath>
+        </defs>
+      </svg>`,
+        `<svg xmlns="http://www.w3.org/2000/svg" width="49" height="95" viewBox="0 0 49 95" fill="none">
+          <g clip-path="url(#clip0_11_96)">
+            <path d="M12.0793 83.2956L36.2896 48.048L12.3533 12.6137" stroke="#E7E7E7" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+          </g>
+          <defs>
+            <clipPath id="clip0_11_96">
+              <rect width="94.2433" height="48.1469" fill="white" transform="matrix(0.00387648 -0.999992 -0.999992 -0.00387648 48.1465 94.4292)"/>
+            </clipPath>
+          </defs>
+        </svg>`
+  
+      ],
       autoplay: false,
       dots: false,
       loop: true,
